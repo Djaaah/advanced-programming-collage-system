b187c9d7b3fa30b8c6dde2bc75add2b6f3b5a659
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2022-10-26 09:00:47 -0300

    Improva Aluno controller class

--- school-system/src/main/java/com/school/system/controller/AlunoController.java
+++ school-system/src/main/java/com/school/system/controller/AlunoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.school.system.exception.AlunoNotFoundException;
@@ -24,40 +25,50 @@ public class AlunoController {
 	private AlunoRepository alunosRepository;
 
 	@PostMapping("/alunos")
-	public Aluno addNewAluno(@RequestBody Aluno aluno) {
+	public @ResponseBody Aluno salvarNovoAluno(@RequestBody Aluno aluno) {
+		// TODO: Verificar se aluno de mesmo CPF/matricula já não existe.
 		return alunosRepository.save(aluno);
 	}
 
 	@GetMapping("/alunos")
-	public List<Aluno> getAllAlunos() {
+	public @ResponseBody List<Aluno> obterTodosAlunos() {
+		// TODO: Usar a classe Pageable para fazer a paginação no frontend
 		return alunosRepository.findAll();
 	}
 
 	@GetMapping("/alunos/{matricula}")
-	public Aluno getAlunosById(@PathVariable Integer matricula) {
+	public @ResponseBody Aluno obterAlunoPelaMatricula(@PathVariable("matricula") Integer matricula) {
 		return alunosRepository.findById(matricula).orElseThrow(() -> new AlunoNotFoundException(matricula));
 	}
 
 	@PutMapping("/alunos/{matricula}")
-	public Aluno updateAlunos(@RequestBody Aluno newAluno, @PathVariable Integer matricula) {
-		return alunosRepository.findById(matricula).map(alunos -> {
-			alunos.setNomeCompleto(newAluno.getNomeCompleto());
-			alunos.setTelefone(newAluno.getTelefone());
-			alunos.setEndereco(newAluno.getEndereco());
-			alunos.setCpf(newAluno.getCpf());
-			alunos.setIdTurma(newAluno.getIdTurma());
-			return alunosRepository.save(alunos);
-		}).orElseThrow(() -> new AlunoNotFoundException(matricula));
+	public @ResponseBody Aluno atualizarAlunoPelaMatricula(@RequestBody Aluno body, @PathVariable("matricula") Integer matricula) {
+		// Só pode existir um único aluno por matrícula
+
+		if (!alunosRepository.existsById(matricula)) {
+			throw new AlunoNotFoundException(matricula);
+		}
+
+		Aluno aluno = alunosRepository.findById(matricula).get();
+
+		aluno.setNomeCompleto(body.getNomeCompleto());
+		aluno.setTelefone(body.getTelefone());
+		aluno.setEndereco(body.getEndereco());
+		aluno.setCpf(body.getCpf());
+		aluno.setIdTurma(body.getIdTurma());
+
+		return alunosRepository.save(aluno);
 	}
 
 	@DeleteMapping("/alunos/{matricula}")
-	public String deleteAlunos(@PathVariable("matricula") Integer matricula) {
+	public @ResponseBody String deleteAlunos(@PathVariable("matricula") Integer matricula) {
 		if (!alunosRepository.existsById(matricula)) {
 			throw new AlunoNotFoundException(matricula);
 		}
 
 		alunosRepository.deleteById(matricula);
 
-		return "Student with matricula: " + matricula + " has been Deleted Success.";
+		return "Student with matricula: " + matricula + " has been deleted successfully!";
 	}
+	
 }
